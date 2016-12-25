43989a0ecce3da6e312a6b6d9ad26915e5bf8fca
samboswell <samuel.bosbetter@gmail.com>
2016-12-25 03:31:59 -0500

    looked at w3 for a sec.

--- app_main.js
+++ app_main.js
@@ -1,8 +1,7 @@
 class MasterButtons extends React.Component {
   render() {
     return (
-      <button type="button" className="studio" style="art_photos/apple.jpg"> Studio Art </button>
-      <input type="image" src="art_photos/box1.jpg" />
+      <button type="button" className="studio" > Studio Art </button>
         <button type="button" className="about"> About </button>
           <button type="button" className="cs"> Computer Science </button>
     )

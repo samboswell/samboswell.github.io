8b0279aab4ce179f7f69f29dc1cfb0cd292a8889
samboswell <samuel.bosbetter@gmail.com>
2017-04-07 09:21:49 -0500

    new website

---
+++ website.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<meta charset='UTF-8'>
+<title>Sam Boswell</title>
+<script src="https://fb.me/react-15.1.0.js"></script>
+<script src="https://fb.me/react-dom-15.1.0.js"></script>
+    <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
+</head>
+<body>
+<h1>Sam Boswell </h1>
+<p>
+ART: <a href="https://samboswell.github.io/art_homepage.html">
+<img border="0" alt="W3Schools" src="art_photos/apple.jpg" width="100" height="100">
+</a>
+
+ABOUT: <a href="https://samboswell.github.io/about_homepage.html">
+<img border="0" alt="W3Schools" src="art_photos/bench.jpg" width="100" height="100">
+</a>
+
+CS: <a href="https://samboswell.github.io/cs_homepage.html">
+<img border="0" alt="W3Schools" src="art_photos/box1.jpg" width="100" height="100">
+</a>
+</p>
+
+</body>
+
+</html>
